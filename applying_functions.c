78d568784623eb0dfac994d07245f744d4a95f45
Dainerx <oussema.benghorbel@esprit.tn>
2019-10-05 16:01:35 +0200

    sums

--- applying_functions.c
+++ applying_functions.c
@@ -5,15 +5,18 @@
 #define MAXVAL 20
 
 
-double f1(double x) {
+double f1(double x)
+{
     return 2.17 * log(x) * cos(x); // not O(1), O is polynomial
 }
 
-double f(double x) {
+double f(double x)
+{
     return 1.17 * x * x;
 }
 //HTOP see
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int n = atoi(argv[1]);
     int p = atoi(argv[2]);
     printf("n = %d\n",n);
@@ -22,7 +25,8 @@ int main(int argc, char *argv[]) {
     double *tt = malloc(sizeof (double) * n);
     srand(getpid());
 
-    for(int i =0; i<n; i++) {
+    for(int i =0; i<n; i++)
+    {
         tt[i] = rand() % MAXVAL;
     }
 
@@ -32,7 +36,8 @@ int main(int argc, char *argv[]) {
     start = clock();
     //seq
     int i =0;
-    for(i =0; i<n; i++) {
+    for(i =0; i<n; i++)
+    {
         //tt[i] = f(tt[i]);
         tt[i] = f1(tt[i]);
     }
@@ -41,24 +46,54 @@ int main(int argc, char *argv[]) {
     printf("seq: for n=%d, time taken=%f\n",n,cpu_time_used_seq);
     //end seq
 
-    for (int pp = 2; pp<17; pp++){
+
+    //sets a chunk value
+    int chunk = n/p; //acts like floor
+    omp_set_num_threads(p);
+    double start_parallel = omp_get_wtime();
+    //start parallel
+    #pragma omp parallel shared(tt,chunk) private (i)
+    {
+        //printf("thread number %d\n",omp_get_thread_num());
+        #pragma omp for schedule(dynamic,chunk) nowait
+        for(i=0; i<n; i++)
+        {
+            //tt[i] = f(tt[i]); //O(1)
+            tt[i] = f1(tt[i]); //the speed up is much higher with f1 since log and cos computations are polynomial; see function.
+        }
+        
+    } //end parralel
+    double end_parallel = omp_get_wtime();
+    double cpu_time_used_parallel = (double) (end_parallel - start_parallel);
+    //speed up ~ 3
+    printf("parallel: for n=%d, p=%d, time taken=%f, speedup=%f\n",n,p,cpu_time_used_parallel,cpu_time_used_seq/cpu_time_used_parallel);
+
+
+    //now we will vary p from 2 to 16 in order to see how the speed varies along with the number of threads
+    
+    printf("Started varying threads:\n");
+    for (int pp = 2; pp<17; pp++)
+    {
         p = pp;
         int chunk = n/p; //acts like floor
         omp_set_num_threads(p);
-        //start parallel
         double start_parallel = omp_get_wtime();
-        #pragma parallel shared(tt,chunk) private (i)
+        //start parallel
+        #pragma omp parallel shared(tt,chunk) private (i)
         {
+            //printf("thread number %d\n",omp_get_thread_num());
             #pragma omp for schedule(dynamic,chunk) nowait
-                for(i=0;i<n;i++) {
-                    //tt[i] = f(tt[i]);
-                    tt[i] = f1(tt[i]); //the speed up is much higher with f1 since log and cos takes time to be computed
-                }
-            printf("%d\n",omp_get_num_threads());
-        }
+            for(i=0; i<n; i++)
+            {
+                //tt[i] = f(tt[i]);
+                tt[i] = f1(tt[i]); //the speed up is much higher with f1 since log and cos takes time to be computed
+            }
+            //printf("from pp=%d, thread %d finish\n",pp,omp_get_thread_num());
+        } //end parralel
         double end_parallel = omp_get_wtime();
         double cpu_time_used_parallel = (double) (end_parallel - start_parallel);
-        printf("parallel: for n=%d, p=%d, time taken=%f, speedup=%f\n",n,omp_get_num_threads(),cpu_time_used_parallel,cpu_time_used_seq/cpu_time_used_parallel);
-        //end parralel
+        printf("parallel: for n=%d, p=%d, time taken=%f, speedup=%f\n",n,p,cpu_time_used_parallel,cpu_time_used_seq/cpu_time_used_parallel);
     }
+    return 0;
+
 }
---
+++ sum_of_array.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <omp.h>
+#include <time.h>
+#include <math.h>
+#define MAXVAL 20
+
+int main(int argc, char *argv[]) {
+    int n = atoi(argv[1]);
+    int p = atoi(argv[2]);
+    printf("n = %d\n", n);
+    printf("p = %d\n", p);
+
+    double *tt = malloc(sizeof (double) * n);
+    srand(getpid());
+    //init array
+    for (int i = 0; i < n; i++) {
+        tt[i] = rand() % MAXVAL;
+    }
+
+    //start eq
+    clock_t start, end;
+    double cpu_time_used_seq;
+    int i = 0;
+    double sum = 0;
+    start = clock();
+    for (i = 0; i < n; i++) {
+        sum += tt[i];
+    }
+    end = clock();
+    cpu_time_used_seq = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("seq: sum=%f\n", sum);
+    printf("seq: for n=%d, time taken=%f\n", n, cpu_time_used_seq);
+    //end seq
+
+    printf("\n======================================\nUsing Parallelism without reduction: \n");
+    sum = 0;
+    double lsum = 0;
+    omp_set_num_threads(p);
+    //start parallel without reduction
+    //this will divide work among threads in non equal way, if the operation had 
+    //a huge cost we might end up having the final thread doing a shit ton of work compared to the other threads
+    double start_parallel = omp_get_wtime();
+#pragma omp parallel private(i,lsum) 
+    {
+#pragma omp for
+        for (i = 0; i < n; i++) {
+            //see what index each thread is working on
+            //printf("thread %d -> %d\n", omp_get_thread_num(), i);
+            lsum += tt[i];
+        }
+        //synchronization section
+#pragma omp critical
+        {
+            sum += lsum;
+        }
+    }
+    //end parallel without reduction
+    double end_parallel = omp_get_wtime();
+    double cpu_time_used_parallel = (double) (end_parallel - start_parallel);
+    printf("parallel without reduction: sum=%f\n", sum);
+    printf("parallel without reduction: for n=%d, p=%d, time taken=%f, speedup=%f\n", n, p, cpu_time_used_parallel, cpu_time_used_seq / cpu_time_used_parallel);
+
+    printf("\n======================================\nUsing Parallelism with reduction: \n");
+    sum = 0;
+    //start parallel with reduction
+    start_parallel = omp_get_wtime();
+#pragma omp parallel for reduction(+:sum)
+    for (i = 0; i < n; i++) {
+        //see what index each thread is working on
+        //printf("thread %d -> %d\n", omp_get_thread_num(), i);
+        sum += tt[i];
+    }
+    //end parallel with reduction
+    end_parallel = omp_get_wtime();
+    cpu_time_used_parallel = (double) (end_parallel - start_parallel);
+    printf("parallel with reduction: sum=%f\n", sum);
+    printf("parallel with reduction: for n=%d, p=%d, time taken=%f, speedup=%f\n", n, p, cpu_time_used_parallel, cpu_time_used_seq / cpu_time_used_parallel);
+
+    printf("\n======================================\nUsing schedule and reduction: \n");
+    sum = 0;
+    int chunk = n / (p * 2); //this is faster than the default scheduling by compiler. n/p would be slower though.
+    printf("Setting chunk to %d\n", chunk);
+    start_parallel = omp_get_wtime();
+#pragma omp parallel shared(chunk) private (i)
+    {
+#pragma omp for reduction(+:sum) schedule(dynamic,chunk)
+        for (i = 0; i < n; i++) {
+            sum += tt[i];
+        }
+    }
+    end_parallel = omp_get_wtime();
+    cpu_time_used_parallel = (double) (end_parallel - start_parallel);
+    printf("parallel with reduction and dynamic scheduling: sum=%f\n", sum);
+    printf("parallel with reduction and dynamic scheduling: for n=%d, p=%d, time taken=%f, speedup=%f\n", n, p, cpu_time_used_parallel, cpu_time_used_seq / cpu_time_used_parallel);
+
+
+    printf("\n======================================\nUsing schedule without reduction: \n");
+    sum = 0;
+    lsum = 0;
+    chunk = n / (p * 2); //this is faster than the default scheduling by compiler. n/p would be slower though.
+    printf("Setting chunk to %d\n", chunk);
+    //should not be faster.
+    start_parallel = omp_get_wtime();
+#pragma omp parallel shared(chunk) private (i,lsum)
+    {
+#pragma omp for schedule(dynamic,chunk)
+        for (i = 0; i < n; i++) {
+            lsum += tt[i];
+        }
+#pragma omp atomic
+        sum += lsum;
+    }
+    end_parallel = omp_get_wtime();
+    cpu_time_used_parallel = (double) (end_parallel - start_parallel);
+    printf("parallel dynamic scheduling without reduction: sum=%f\n", sum);
+    printf("parallel dynamic scheduling without reduction: for n=%d, p=%d, time taken=%f, speedup=%f\n", n, p, cpu_time_used_parallel, cpu_time_used_seq / cpu_time_used_parallel);
+
+}
