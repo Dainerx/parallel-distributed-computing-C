6aaeb9e22e0c22d6133b077038a13846091d9162
chekinih <chekini_hakima@yahoo.fr>
2020-01-22 21:28:01 +0100

    mpi parallelized scater gather

--- MultMat/mpi_parallelized_scatter_gather.c
+++ MultMat/mpi_parallelized_scatter_gather.c
@@ -4,49 +4,28 @@
 #include <mpi.h>
 #include <omp.h>
 #include <time.h>
-#define LINES_A 100
-#define COLUMNS_A 200
-#define LINES_B 200
-#define COLUMNS_B 100
-
+#include "mpi_utils.h"
 #include "matrix_util.h"
+#include "metrics.h"
+#include "solver.h"
 
-void unflat_Lin(int *A, int lines, int columns, int **A_unflatted)
-{
-  int i, j;
-#pragma omp parallel for private(j) schedule(guided) shared(A_unflatted)
-  for( i = 0; i < lines; i++)
-  {
-    for( j = 0; j < columns; j++)
-    {
-        A_unflatted[i][j] = A[i * columns + j];
-    }
-  }
-}
+// Variable globale contient l'input
+struct CmdInputDistributed ci;
 
-void unflat_Col(int *B, int lines, int columns, int **B_unflatted)
+/*
+  Cette fonction permet de faire une multiplication séquentielle de deux matrices A et B
+*/
+double mpi_mult_seq(int **mat_A, int **mat_B, int **mat_C)
 {
-  int i, j;
-#pragma omp parallel for private(j) schedule(guided) shared(B_unflatted)
-  for( i = 0; i < lines; i++)
-  {
-    for( j = 0; j< columns; j++)
-    {
-        B_unflatted[i][j] = B[j * lines + i];
-    }
-  }
-}
-double sequential_mult(int **mat_A, int **mat_B, int **mat_C)
-{
-    double start, end, cpu_time_used;
-    start = omp_get_wtime();
+  double start, end, cpu_time_used;
+  start = omp_get_wtime();
     int sum;
-    for (int i = 0; i < LINES_A; i++)
+    for (int i = 0; i < ci.lines_a; i++)
     {
-        for (int j = 0; j < COLUMNS_B; j++)
+        for (int j = 0; j < ci.columns_b; j++)
         {
             sum = 0;
-            for (int k = 0; k < COLUMNS_A; k++)
+            for (int k = 0; k < ci.columns_a; k++)
             {
                 sum += (mat_A[i][k] * mat_B[k][j]);
             }
@@ -58,109 +37,82 @@ double sequential_mult(int **mat_A, int **mat_B, int **mat_C)
     return cpu_time_used;
 }
 
-bool mat_equal(int **A, int** B)
+/*
+  Cette fonction permet de convertir des matrices de deux dimensions en une dimension
+  Elle prend en paramètre deux matrices A et B, La matrice A va être transformée en une matrice 1D présentée ligne après ligne
+  Et la matrice B va être transformée en une matrice 1D présentée colonne après colonne
+
+*/
+void convertMat_parralel(int **matrixA, int **matrixB, int *a, int *b)
 {
-  bool correct = true;
-  for (int i = 0; i < LINES_A; i++)
-  {
-      for (int j = 0; j < COLUMNS_B; j++)
-      {
-        if (A[i][j] != B[i][j])
-        {
-            printf("Found no equal cell: (%d,%d) a(%d,%d)=%d ; b(%d,%d)=%d\n", i, j, i, j, A[i][j], i, j, B[i][j]);
-            correct = false;
-        }
-      }
-  }
-  return correct;
-}
-void convertMat(int **matrixA, int **matrixB, int *a, int *b)
-{
-  int i, j;
+  int i,j;
   #pragma omp parallel
   {
     #pragma omp parallel for schedule(guided) private(j) shared(a)
-        for ( i = 0; i < LINES_A; i++)
+    for ( i = 0; i < ci.lines_a; i++)
+    {
+        for ( j = 0; j < ci.columns_a; j++)
         {
-            for (j = 0; j < COLUMNS_A; j++)
-            {
-                a[i * COLUMNS_A + j] = matrixA[i][j];
-            }
+            a[i * ci.columns_a + j] = matrixA[i][j];
         }
-
+    }
     #pragma omp parallel for schedule(guided) private(j) shared(b)
-        for (i = 0; i < LINES_B; i++)
+    for ( i = 0; i < ci.lines_b; i++)
+    {
+        for ( j = 0; j < ci.columns_b; j++)
         {
-            for (j = 0; j < COLUMNS_B; j++)
-            {
-                b[j * LINES_B + i] = matrixB[i][j];
-            }
+            b[j * ci.lines_b + i] = matrixB[i][j];
         }
+    }
   }
 }
 
- void mult(int **A, int **B, int **C, int linesA, int columnsA,int linesB, int columnsB)
- {
-   int i, j, k, sum;
-   #pragma omp parallel for schedule(guided) collapse(2) private(j, k, sum) shared(A, B, C)
-   for ( i = 0; i < linesA; i++)
-   {
-       for ( j = 0; j < columnsB; j++)
-       {
-           sum = 0;
-           for ( k = 0; k < columnsA; k++)
-           {
-               sum += (A[i][k] * B[k][j]);
-           }
-           C[i][j] = sum;
-       }
-   }
- }
-
+ /*
+   Cette fontion permet de faire la multiplication d'une partie de la matrice A reçue par chaque machine avec la matrice B connue par toutes les machines
+ */
 void get_res_lines_C(int *local_res,int *received_part_A,int *flatB, int nbr_linesA_received)
 {
-  int i, j;
-  int **partA_unflatted = malloc_mat(nbr_linesA_received, COLUMNS_A);
-  int **B_unflatted = malloc_mat(LINES_B, COLUMNS_B);
-  int **C = malloc_mat(nbr_linesA_received, COLUMNS_B);
+  int k = 0;
+  int c = 0;
+  int t, i;
+  int sizeLineA = 0;
 
-  unflat_Lin(received_part_A, nbr_linesA_received, COLUMNS_A, partA_unflatted);
-
-  unflat_Col(flatB,LINES_B ,COLUMNS_B, B_unflatted);
-
-  mult(partA_unflatted, B_unflatted, C, nbr_linesA_received, COLUMNS_A, LINES_B, COLUMNS_B);
-
-#pragma omp parallel for private(j)shared(local_res)
-  for( i = 0; i < nbr_linesA_received; i++)
+  for(int linesA = 0; linesA < nbr_linesA_received; linesA++)
   {
-      for ( j = 0; j < COLUMNS_B; j++)
-      {
-          local_res[i * COLUMNS_B + j] = C[i][j];
-      }
-  }
-  free_mat(partA_unflatted, nbr_linesA_received);
-  free_mat(B_unflatted,LINES_B );
-  free_mat(C,nbr_linesA_received );
-}
-
-void display_linear_mat(int *mat, int lines, int columns)
-{
-  for(int i=0; i<lines*columns; i++)
-  {
-    if(i%columns ==0)
+    for(k = 0; k<  ci.lines_b * ci.columns_b; k+= ci.lines_b)
     {
-      printf("\n");
+      t = 0;
+      for ( i = 0; i < ci.columns_a; i++)
+      {
+        t += received_part_A[i+sizeLineA] * flatB[i + k];
+      }
+      local_res[c] = t;
+      c += 1;
     }
-    printf("%d\t", mat[i]);
+    sizeLineA += ci.columns_a;
   }
 }
+
 int main(int argc, char **argv)
 {
     const int root = 0;
     int rank, world_size;
+    int rc = 0;
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size( MPI_COMM_WORLD, &world_size);
+
+    ci = read_input(rank, world_size, argc, argv);
+    if (check_input(rank, ci) == false)
+    {
+        MPI_Abort(MPI_COMM_WORLD, rc);
+        exit(1);
+    }
+    else
+    {
+        print_colored(rank, 2, "    OK: Input valid.");
+        print_colored(rank, 0, "\n    A: (%d,%d)\n    B: (%d,%d)\n", ci.lines_a, ci.columns_a, ci.lines_b, ci.columns_b);
+    }
 
     srand(time(NULL)+rank);
     int **mat_A;
@@ -168,92 +120,99 @@ int main(int argc, char **argv)
     int *flatA;
     int* res_final;
     int **mat_C;
+    double time_used_seq;
 
     double start, end, time_used_mpi;
-    if((LINES_A * COLUMNS_A) % world_size != 0)
+    if((ci.lines_a * ci.columns_a) % world_size != 0)
     {
       printf("Le nombre de machines doit etre divisible par LINES_A * COLUMNS_A\n");
       return -1;
     }
-    if((LINES_A * COLUMNS_A / world_size) % COLUMNS_A != 0)
+    // S'assurer que chaque machine reçoie un certain nombre de lignes de la matrice A et non juste une partie d'une ligne
+    if((ci.lines_a * ci.columns_a / world_size) % ci.columns_a  != 0)
     {
       printf(" lingesA * colunnesA /nombre de machines doit etre divisible sur colonnesA\n");
       return -2;
     }
-    int *flatB = malloc((LINES_B * COLUMNS_B) * sizeof(int));
+    int *flatB = malloc((ci.lines_b * ci.columns_b ) * sizeof(int));
+
+    // Le machine root va allouer et initialiser les deux matrices A et B. Ensuite, elle va distribuer la matrice B à toutes les autres machines, et envoie
+    // pour chaque machine sa partie de la matrice A
     if(rank == root)
     {
-      mat_A = malloc_mat(LINES_A, COLUMNS_A);
-      mat_B = malloc_mat(LINES_B, COLUMNS_B);
-      mat_C = malloc_mat(LINES_A, COLUMNS_B);
+      mat_A = malloc_mat(ci.lines_a, ci.columns_a);
+      mat_B = malloc_mat(ci.lines_b, ci.columns_b);
+      mat_C = malloc_mat(ci.lines_a, ci.columns_b);
 
-      fill_mat(mat_A, LINES_A, COLUMNS_A);
-      fill_mat(mat_B, LINES_B, COLUMNS_B);
+      fill_mat(mat_A, ci.lines_a, ci.columns_a);
+      fill_mat(mat_B, ci.lines_b, ci.columns_b);
 
-      int time_seq = sequential_mult(mat_A, mat_B, mat_C);
-      start= MPI_Wtime();
-      flatA =  malloc((LINES_A * COLUMNS_A) * sizeof(int));
-      convertMat(mat_A, mat_B,flatA,flatB);
+      time_used_seq = mpi_mult_seq(mat_A, mat_B, mat_C);
+      start = MPI_Wtime();
+
+      flatA =  malloc((ci.lines_a *  ci.columns_a) * sizeof(int));
+      // Conversion des matrices en une dimension
+      convertMat_parralel(mat_A, mat_B,flatA,flatB);
 
       // Le root va allouer la taille mémoire pour contenir le résultat de la multiplication
-      res_final = malloc((LINES_A*COLUMNS_B)*sizeof(int));
+      res_final = malloc((ci.lines_a * ci.columns_b)*sizeof(int));
     }
 
+    // Toutes les machines vont attendre que la machine root fini de remplir les matrices A et B
     MPI_Barrier(MPI_COMM_WORLD);
 
-    // Envoie de la matrice B a tout le monde; B sera partagée
-    MPI_Bcast(flatB,LINES_B*COLUMNS_B,MPI_INT,root,MPI_COMM_WORLD);
+    // Envoie de la matrice B a tout le monde par la machine root; B sera partagée
+    MPI_Bcast(flatB,ci.lines_b * ci.columns_b, MPI_INT, root, MPI_COMM_WORLD);
 
-    int length_revceived_A = COLUMNS_A*LINES_A / world_size;
-
-    // Chacun alloue un tableau de taille COLUMNS_A*LINES_A / world_size pour recevoir sa partie de la matrice A
+    int length_revceived_A = ci.columns_a * ci.lines_a / world_size;
+    // Toutes les machines vont allouer un espace pour recevoir leur partie de la matrice A
     int* received_part_A = (int *)malloc(length_revceived_A *sizeof(int));
 
-    // Le root envoie à toutes les machines y compris lui sa part de la matrice A
+    // Le root envoie à toutes les machines sa part de la matrice A y compris lui
     if(received_part_A)
     {
       MPI_Scatter(flatA,length_revceived_A, MPI_INT, received_part_A, length_revceived_A, MPI_INT, root, MPI_COMM_WORLD);
     }
-    int nbr_linesA_received = length_revceived_A / COLUMNS_A;
+    int nbr_linesA_received = length_revceived_A / ci.columns_a;
 
-    // Chacun dispose désormais de la matrice B en 1D (B est partagée) et un nombre x de lignes de la matrice A
-    // Chacun doit avoir comme résultat x lignes de la matrice C
+    // Chaque machine dispose désormais de la matrice B en 1D (B est partagée) et un nombre x de lignes de la matrice A
+    // Chacune doit avoir comme résultat x lignes de la matrice C
     // Ensuite tout le monde envoie sa partie calculée au root (gather)
-    int* local_res = (int*) malloc(nbr_linesA_received*COLUMNS_B*sizeof(int));
 
-    // Chacun calcule x lignes de la matrice C
+    // Chaque machine alloue un espace pour mettre le résultat du calcul de la partie de A reçu * la matrice B
+    int* local_res = (int*) malloc(nbr_linesA_received * ci.columns_b * sizeof(int));
+
+    // Chacune calcule x lignes de la matrice C
     get_res_lines_C(local_res,received_part_A,flatB, nbr_linesA_received);
 
     // Tout le monde envoie sa partie calculée au root (gather)
-    //Le root rencoi le résultat de toutes les machines
-    MPI_Gather(local_res,nbr_linesA_received*COLUMNS_B , MPI_INT, res_final, nbr_linesA_received*COLUMNS_B, MPI_INT, root, MPI_COMM_WORLD );
+    //Le root rençoi le résultat de toutes les machines
+    MPI_Gather(local_res, nbr_linesA_received * ci.columns_b, MPI_INT, res_final, nbr_linesA_received * ci.columns_b, MPI_INT, root, MPI_COMM_WORLD );
 
+    // Toutes les machines vont attendre que le root reçoi les résulats de toutes les machines pour que
+    // la machine root désalloue ce qui a ete alloue
     MPI_Barrier(MPI_COMM_WORLD);
+
     if(rank == root)
     {
       end= MPI_Wtime();
       time_used_mpi = end - start;
-      printf("temps USED MPI %f:\n", time_used_mpi);
-      int ** CC = malloc_mat( LINES_A, COLUMNS_B );
-      unflat_Lin(res_final, LINES_A, COLUMNS_B, CC );
+      printf("\n");
+      printf("    Time used MPI using scatter and gather: %f:\n", time_used_mpi);
+      printf("    Time used in sequential: %f:\n", time_used_seq);
 
-      if(!mat_equal(mat_C, CC))
+     if(!result_correct( ci.lines_a, ci.columns_b,res_final, mat_C))
       {
         printf("error mat not equal \n");
         return -1;
       }
-      else{
-        printf("bravo \n");
-      }
+      // Desallocation
+      free_mat(mat_A, ci.lines_a );
+      free_mat(mat_B, ci.lines_b);
+      free(flatA);
     }
 
     free(flatB);
-    if(rank == root)
-       {
-         free_mat(mat_A, LINES_A );
-         free_mat(mat_B, LINES_B);
-         free(flatA);
-      }
     MPI_Finalize();
     return EXIT_SUCCESS;
 }
